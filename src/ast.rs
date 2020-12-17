209f6ddd74eba097d94556b3a77af64727c7df72
Tom Forbes <tom@tomforb.es>
2020-12-17 16:11:17 +0000

    WIP

--- src/ast.rs
+++ src/ast.rs
@@ -27,36 +27,40 @@ pub enum UnaryOp {
 }
 
 #[derive(Debug)]
-pub enum Expression {
-    Arithmetic { left: Box<Expression>, op: ArithmeticOp, right: Box<Expression> },
-    Relation { left: Box<Expression>, op: RelationOp, right: Box<Expression> },
+pub enum Expression<'a> {
+    Arithmetic { left: &'a Expression<'a>, op: ArithmeticOp, right: &'a Expression<'a> },
+    Relation { left: &'a Expression<'a>, op: RelationOp, right: &'a Expression<'a> },
 
-    Ternary { condition: Box<Expression>, left: Box<Expression>, right: Box<Expression> },
-    Or { left: Box<Expression>, right: Box<Expression> },
-    And { left: Box<Expression>, right: Box<Expression> },
-    Unary { op: UnaryOp, right: Box<Expression> },
+    Ternary { condition: &'a Expression<'a>, left: &'a Expression<'a>, right: &'a Expression<'a> },
+    Or { left: &'a Expression<'a>, right: &'a Expression<'a> },
+    And { left: &'a Expression<'a>, right: &'a Expression<'a> },
+    Unary { op: UnaryOp, right: &'a Expression<'a> },
 
-    Member { primary: Box<Expression>, member: Member },
+    Member { primary: &'a Expression<'a>, member: &'a Member<'a> },
 
-    Function { identifier: String, arguments: Vec<Box<Expression>> },
-    List { members: Vec<Box<Expression>> },
-    Map {fields: Vec<(Box<Expression>, Box<Expression>)>},
+    Function { identifier: String, arguments: Vec<&'a Expression<'a>> },
+    List { members: Vec<&'a Expression<'a>> },
+    Map {fields: Vec<(&'a Expression<'a>, &'a Expression<'a>)>},
 
-    Literal(Literal),
+    Literal(Literal<'a>),
     Ident(String),
 }
 
 #[derive(Debug)]
-pub enum Member {
+pub enum Member<'a> {
     Attribute { identifier: String },
-    Function { identifier: String, arguments: Vec<Box<Expression>> },
-    Index { expression: Box<Expression> },
-    Fields { fields: Vec<(String, Box<Expression>)> },
+    Function { identifier: String, arguments: Vec<Expression<'a>> },
+    Index { expression: Expression<'a> },
+    Fields { fields: Vec<(String, Expression<'a>)> },
 }
 
 
 #[derive(Debug)]
-pub enum Literal {
+pub enum Literal<'a> {
     Int(i32),
     UInt(u32),
+    Float(f64),
+    String(&'a str),
+    Bool(bool),
+    Null,
 }
--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -4,7 +4,9 @@ use common_expression_language::ast::Expression;
 use std::mem::size_of;
 
 fn main() {
-    println!("{:?}", cel::ExpressionParser::new().parse("a.b(c + 3 / 2,) == 2").unwrap_or_else(|e| panic!("{}", e)));
+    println!("{:?}", cel::ExpressionParser::new().parse("b(c + 3 / 2,) == 2").unwrap_or_else(|e| panic!("{}", e)));
+    println!("{:?}", cel::ExpressionParser::new().parse(r"has(account.properties.id) && (type(account.properties.id) == string || type(account.properties.id) == list(string))").unwrap_or_else(|e| panic!("{}", e)));
+    println!("{:?}", cel::ExpressionParser::new().parse("-1E3").unwrap_or_else(|e| panic!("{}", e)));
     println!("{:?}", cel::ExpressionParser::new().parse("[1,2,3, abc, ]").unwrap_or_else(|e| panic!("{}", e)));
     println!("{:?}", cel::ExpressionParser::new().parse("Account{user_id: 123}").unwrap_or_else(|e| panic!("{}", e)));
     println!("{:?}", cel::ExpressionParser::new().parse("{1 + a: 3}").unwrap_or_else(|e| panic!("{}", e)));
