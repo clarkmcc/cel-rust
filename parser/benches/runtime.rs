dcdc24a64a70d44c074e04b840df3649f16fced5
Alex Snaps <alex@wcgw.dev>
2024-07-13 06:40:56 -0400

    Added bench

--- parser/benches/runtime.rs
+++ parser/benches/runtime.rs
@@ -1,7 +1,8 @@
-use cel_parser::parse_string;
+use cel_parser::{parse_bytes, parse_string};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 pub fn parse_string_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("string parsing");
     let expressions = vec![
         ("text", "\"text\""),
         ("raw", "r\"text\""),
@@ -10,10 +11,25 @@ pub fn parse_string_benchmark(c: &mut Criterion) {
         ("single oct escape sequence", "\"\\015\""),
     ];
 
-    for (name, expr) in black_box(&expressions) {
-        c.bench_function(name, |b| b.iter(|| parse_string(expr)));
+    for (name, expr) in black_box(expressions) {
+        group.bench_function(name, |b| b.iter(|| parse_string(expr)));
     }
+    group.finish()
 }
 
-criterion_group!(benches, parse_string_benchmark);
+pub fn parse_bytes_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("bytes parsing");
+    let expressions = vec![
+        ("bytes", "text"),
+        ("single hex escape sequence", "x0D"),
+        ("single oct escape sequence", "015"),
+    ];
+
+    for (name, expr) in black_box(expressions) {
+        group.bench_function(name, |b| b.iter(|| parse_bytes(expr)));
+    }
+    group.finish()
+}
+
+criterion_group!(benches, parse_string_benchmark, parse_bytes_benchmark);
 criterion_main!(benches);
