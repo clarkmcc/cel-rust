90463bae2eda8c93b08e1dce17f118304aadc1ea
Clark McCauley <clarkmccauley@gmail.com>
2024-10-30 09:43:11 -0600

    Support `.map` over map (#105)

--- interpreter/src/functions.rs
+++ interpreter/src/functions.rs
@@ -314,6 +314,16 @@ pub fn map(
             }
             Value::List(Arc::new(values))
         }
+        Value::Map(map) => {
+            let mut values = Vec::with_capacity(map.map.len());
+            let mut ptx = ftx.ptx.new_inner_scope();
+            for (key, _) in map.map.iter() {
+                ptx.add_variable_from_value(ident.clone(), key.clone());
+                let value = ptx.resolve(&expr)?;
+                values.push(value);
+            }
+            Value::List(Arc::new(values))
+        }
         _ => return Err(this.error_expected_type(ValueType::List)),
     }
     .into()
@@ -612,6 +622,10 @@ mod tests {
             (
                 "nested map",
                 "[[1, 2], [2, 3]].map(x, x.map(x, x * 2)) == [[2, 4], [4, 6]]",
+            ),
+            (
+                "map to list",
+                r#"{'John': 'smart'}.map(key, key) == ['John']"#,
             ),
         ]
         .iter()
