c3c0197b66a673e522acb4f60707e6a74b23f4fb
Alex Snaps <alex@wcgw.dev>
2025-05-29 16:55:02 -0400

    parser parsing strings
    Signed-off-by: Alex Snaps <alex@wcgw.dev>

--- antlr/src/lib.rs
+++ antlr/src/lib.rs
@@ -1,1 +1,9 @@
+#[allow(clippy::all)]
 mod gen;
+
+mod ast;
+mod reference;
+
+#[allow(non_snake_case)]
+mod parser;
+pub use parser::*;
---
+++ antlr/src/reference.rs
@@ -0,0 +1,7 @@
+#[derive(Debug)]
+pub enum Val {
+    String(String),
+    Boolean(bool),
+    Int(i64),
+    UInt(u64),
+}
