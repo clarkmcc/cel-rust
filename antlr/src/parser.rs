4f003a8d2a1a3f87422b77e791b7767cd29268e6
Alex Snaps <alex@wcgw.dev>
2025-05-30 12:27:36 -0400

    feat(antlr): Expr are now ID'ed
    Signed-off-by: Alex Snaps <alex@wcgw.dev>

--- antlr/src/parser.rs
+++ antlr/src/parser.rs
@@ -1,4 +1,4 @@
-use crate::ast::Expr;
+use crate::ast::{Expr, IdedExpr};
 use crate::gen::{
     BoolFalseContext, BoolTrueContext, CalcContext, CalcContextAttrs, ConditionalAndContext,
     ConditionalOrContext, ConstantLiteralContext, ConstantLiteralContextAttrs, ExprContext,
@@ -13,26 +13,28 @@ use antlr4rust::parser::ParserNodeType;
 use antlr4rust::token::Token;
 use antlr4rust::tree::{ParseTree, ParseTreeVisitorCompat, VisitChildren};
 use antlr4rust::InputStream;
-use std::ops::Deref;
 use std::mem;
+use std::ops::Deref;
 
 #[derive(Debug)]
 pub struct ParserError {}
 
 pub struct Parser {
     ast: ast::Ast,
+    helper: ParserHelper,
 }
 
 impl Parser {
     pub fn new() -> Self {
         Self {
             ast: ast::Ast {
-                expr: Expr::default(),
+                expr: IdedExpr::default(),
             },
+            helper: ParserHelper::default(),
         }
     }
 
-    pub fn parse(mut self, source: &str) -> Result<ast::Expr, ParserError> {
+    pub fn parse(mut self, source: &str) -> Result<IdedExpr, ParserError> {
         let stream = InputStream::new(source);
         let mut lexer = gen::CELLexer::new(stream);
         lexer.remove_error_listeners();
@@ -54,7 +56,7 @@ impl Default for Parser {
 
 impl ParseTreeVisitorCompat<'_> for Parser {
     type Node = gen::CELParserContextType;
-    type Return = ast::Expr;
+    type Return = IdedExpr;
     fn temp_result(&mut self) -> &mut Self::Return {
         &mut self.ast.expr
     }
@@ -65,13 +67,8 @@ impl ParseTreeVisitorCompat<'_> for Parser {
         mem::take(self.temp_result())
     }
 
-    fn aggregate_results(&self, aggregate: Self::Return, next: Self::Return) -> Self::Return {
-        println!("{aggregate:?}");
-        // todo! remove this crap eventually when all nodes build properly into an Expr
-        match next {
-            Expr::Unspecified => aggregate,
-            _ => next,
-        }
+    fn aggregate_results(&self, _aggregate: Self::Return, next: Self::Return) -> Self::Return {
+        next
     }
 }
 
@@ -124,7 +121,7 @@ impl gen::CELVisitorCompat<'_> for Parser {
                     op.get_text(),
                     ctx.calc(1).unwrap().deref().get_text()
                 );
-                ast::Expr::Call
+                self.helper.next_expr(Expr::Call)
             }
         }
     }
@@ -144,12 +141,12 @@ impl gen::CELVisitorCompat<'_> for Parser {
     fn visit_Int(&mut self, ctx: &IntContext<'_>) -> Self::Return {
         let string = ctx.get_text();
         let val = if let Some(string) = string.strip_prefix("0x") {
-                i64::from_str_radix(string, 16)
+            i64::from_str_radix(string, 16)
         } else {
             string.parse::<i64>()
         }
         .unwrap();
-        Expr::Literal(Val::Int(val))
+        self.helper.next_expr(Expr::Literal(Val::Int(val)))
     }
 
     fn visit_Uint(&mut self, ctx: &UintContext<'_>) -> Self::Return {
@@ -161,19 +158,45 @@ impl gen::CELVisitorCompat<'_> for Parser {
             string.parse::<u64>()
         }
         .unwrap();
-        Expr::Literal(Val::UInt(val))
+        self.helper.next_expr(Expr::Literal(Val::UInt(val)))
     }
 
     fn visit_String(&mut self, ctx: &StringContext<'_>) -> Self::Return {
-        Expr::Literal(Val::String(ctx.get_text()))
+        self.helper
+            .next_expr(Expr::Literal(Val::String(ctx.get_text())))
     }
 
     fn visit_BoolTrue(&mut self, _ctx: &BoolTrueContext<'_>) -> Self::Return {
-        Expr::Literal(Val::Boolean(true))
+        self.helper.next_expr(Expr::Literal(Val::Boolean(true)))
     }
 
     fn visit_BoolFalse(&mut self, _ctx: &BoolFalseContext<'_>) -> Self::Return {
-        Expr::Literal(Val::Boolean(false))
+        self.helper.next_expr(Expr::Literal(Val::Boolean(false)))
+    }
+}
+
+pub struct ParserHelper {
+    next_id: u64,
+}
+
+impl Default for ParserHelper {
+    fn default() -> Self {
+        Self { next_id: 1 }
+    }
+}
+
+impl ParserHelper {
+    fn next_id(&mut self) -> u64 {
+        let id = self.next_id;
+        self.next_id += 1;
+        id
+    }
+
+    pub fn next_expr(&mut self, expr: Expr) -> IdedExpr {
+        IdedExpr {
+            id: self.next_id(),
+            expr,
+        }
     }
 }
 
@@ -185,10 +208,10 @@ mod tests {
 
     struct TestInfo {
         // I contains the input expression to be parsed.
-        i: String,
+        i: &'static str,
 
         // P contains the type/id adorned debug output of the expression tree.
-        p: String,
+        p: &'static str,
         // E contains the expected error output for a failed parse, or "" if the parse is expected to be successful.
         // e: String,
 
@@ -205,48 +228,48 @@ mod tests {
     fn test() {
         let test_cases: [TestInfo; 11] = [
             TestInfo {
-                i: r#""A""#.to_string(),
-                p: r#""A"^#1:*expr.Constant_StringValue#"#.to_string(),
+                i: r#""A""#,
+                p: r#""A"^#1:*expr.Constant_StringValue#"#,
             },
             TestInfo {
-                i: r#"true"#.to_string(),
-                p: r#"true^#1:*expr.Constant_BoolValue#"#.to_string(),
+                i: r#"true"#,
+                p: r#"true^#1:*expr.Constant_BoolValue#"#,
             },
             TestInfo {
-                i: r#"false"#.to_string(),
-                p: r#"false^#1:*expr.Constant_BoolValue#"#.to_string(),
+                i: r#"false"#,
+                p: r#"false^#1:*expr.Constant_BoolValue#"#,
             },
             TestInfo {
-                i: "0".to_string(),
-                p: "0^#1:*expr.Constant_Int64Value#".to_string(),
+                i: "0",
+                p: "0^#1:*expr.Constant_Int64Value#",
             },
             TestInfo {
-                i: "42".to_string(),
-                p: "42^#1:*expr.Constant_Int64Value#".to_string(),
+                i: "42",
+                p: "42^#1:*expr.Constant_Int64Value#",
             },
             TestInfo {
-                i: "0xF".to_string(),
-                p: "15^#1:*expr.Constant_Int64Value#".to_string(),
+                i: "0xF",
+                p: "15^#1:*expr.Constant_Int64Value#",
             },
             TestInfo {
-                i: "0u".to_string(),
-                p: "0u^#1:*expr.Constant_Uint64Value#".to_string(),
+                i: "0u",
+                p: "0u^#1:*expr.Constant_Uint64Value#",
             },
             TestInfo {
-                i: "23u".to_string(),
-                p: "23u^#1:*expr.Constant_Uint64Value#".to_string(),
+                i: "23u",
+                p: "23u^#1:*expr.Constant_Uint64Value#",
             },
             TestInfo {
-                i: "24u".to_string(),
-                p: "24u^#1:*expr.Constant_Uint64Value#".to_string(),
+                i: "24u",
+                p: "24u^#1:*expr.Constant_Uint64Value#",
             },
             TestInfo {
-                i: "0xFu".to_string(),
-                p: "15u^#1:*expr.Constant_Uint64Value#".to_string(),
+                i: "0xFu",
+                p: "15u^#1:*expr.Constant_Uint64Value#",
             },
             TestInfo {
-                i: "-1".to_string(),
-                p: "-1^#1:*expr.Constant_Int64Value#".to_string(),
+                i: "-1",
+                p: "-1^#1:*expr.Constant_Int64Value#",
             },
         ];
 
@@ -254,7 +277,7 @@ mod tests {
             let parser = Parser::new();
             let result = parser.parse(&test_case.i);
             assert_eq!(
-                to_go_like_string(result.unwrap()),
+                to_go_like_string(result.unwrap().expr),
                 test_case.p,
                 "Expr `{}` failed",
                 test_case.i
@@ -265,7 +288,7 @@ mod tests {
     fn to_go_like_string(expr: Expr) -> String {
         match expr {
             Expr::Unspecified => "*expr.Unspecified".to_string(),
-            Expr::Call => "".to_string(),
+            Expr::Call => "*expr.Expr_CallExpr".to_string(),
             Expr::Comprehension => "".to_string(),
             Expr::Ident => "".to_string(),
             Expr::List => "".to_string(),
