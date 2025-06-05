9f7a17a6d72c2ad64aeb145017813eaae68e2766
Alex Snaps <alex@wcgw.dev>
2025-06-05 15:25:16 -0400

    feat(macros): exists macro expansion
    Signed-off-by: Alex Snaps <alex@wcgw.dev>

--- antlr/src/macros.rs
+++ antlr/src/macros.rs
@@ -1,4 +1,5 @@
-use crate::ast::{Expr, IdedExpr};
+use crate::ast::{CallExpr, ComprehensionExpr, Expr, IdedExpr};
+use crate::reference::Val::Boolean;
 use crate::ParserHelper;
 
 pub fn has_macro_expander(
@@ -20,4 +21,50 @@ pub fn has_macro_expander(
         }
         _ => panic!("Not a select expression"),
     }
+}
+
+pub fn exists_macro_expander(
+    helper: &mut ParserHelper,
+    target: Option<IdedExpr>,
+    mut args: Vec<IdedExpr>,
+) -> IdedExpr {
+    let mut arguments = vec![args.remove(1)];
+    let v = match args.remove(0).expr {
+        Expr::Ident(ident) => ident,
+        _ => panic!("Not an ident expression"),
+    };
+
+    let init = helper.next_expr(Expr::Literal(Boolean(false)));
+    let result_binding = "@result".to_string();
+    let accu_ident = helper.next_expr(Expr::Ident(result_binding.clone()));
+    let arg = helper.next_expr(Expr::Call(CallExpr {
+        func_name: "!_".to_string(),
+        target: None,
+        args: vec![accu_ident],
+    }));
+    let condition = helper.next_expr(Expr::Call(CallExpr {
+        func_name: "@not_strictly_false".to_string(),
+        target: None,
+        args: vec![arg],
+    }));
+
+    arguments.insert(0, helper.next_expr(Expr::Ident(result_binding.clone())));
+    let step = helper.next_expr(Expr::Call(CallExpr {
+        func_name: "_||_".to_string(),
+        target: None,
+        args: arguments,
+    }));
+
+    let result = helper.next_expr(Expr::Ident(result_binding.clone()));
+
+    helper.next_expr(Expr::Comprehension(ComprehensionExpr {
+        iter_range: Box::new(target.unwrap()),
+        iter_var: v,
+        iter_var2: None,
+        accu_var: result_binding,
+        accu_init: init.into(),
+        loop_cond: condition.into(),
+        loop_step: step.into(),
+        result: result.into(),
+    }))
 }
